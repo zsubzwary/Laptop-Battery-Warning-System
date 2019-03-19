20820f591abc501eead8a22435a2446748bd40cf
zsubzwary <31027252+zsubzwary@users.noreply.github.com>
2019-03-19 16:40:15 +0500

    Fixed a typo Changing execution sequence

--- Laptop Battery Warning System/Form1.cs
+++ Laptop Battery Warning System/Form1.cs
@@ -6,6 +6,8 @@ namespace Laptop_Battery_Warning_System
 {
     public partial class Form1 : Form
     {
+        private Thread thread;
+
         public Form1()
         {
             InitializeComponent();
@@ -13,7 +15,31 @@ namespace Laptop_Battery_Warning_System
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            new Thread(() =>
+        }
+
+        private void showNotification(String status, double percentage)
+        {
+            var notification = new System.Windows.Forms.NotifyIcon()
+            {
+                Visible = true,
+                Icon = System.Drawing.SystemIcons.Information,
+                BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info,
+                BalloonTipTitle = "Zaki Battery Warning App",
+                BalloonTipText = $"Your battery percentage is {percentage}%\nPlease take suitable action for healthier battery life !!",
+            };
+
+            // Display for 20 seconds
+            notification.ShowBalloonTip(20000);
+
+            notification.Dispose();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            this.Hide();
+
+            thread = new Thread(() =>
             {
                 while (true)
                 {
@@ -32,33 +58,17 @@ namespace Laptop_Battery_Warning_System
                     {
                         showNotification(batterystatus, bt);
 
-                        MessageBox.Show("Battery charge status : " + batterystatus + " - life" + batterylife);
+                        MessageBox.Show($"Battery Status is ");
                     }
+                    Thread.Sleep(2000);
                 }
-            }).Start();
+            });
+            thread.Start();
         }
 
-        private void showNotification(String status, double percentage)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            var notification = new System.Windows.Forms.NotifyIcon()
-            {
-                Visible = true,
-                Icon = System.Drawing.SystemIcons.Information,
-                BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info,
-                BalloonTipTitle = "Zaki Battery Warning App",
-                BalloonTipText = $"Your batter percentage is {percentage}%\nPlease take suitable action for healthier battery life !!",
-            };
-
-            // Display for 20 seconds
-            notification.ShowBalloonTip(20000);
-
-            notification.Dispose();
-        }
-
-        private void Form1_Shown(object sender, EventArgs e)
-        {
-            this.Visible = false;
-            this.Hide();
+            thread.Abort();
         }
     }
 }
